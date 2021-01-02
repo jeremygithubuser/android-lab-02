a6c251c9548db6e4e01350edbbae6e086f25d8a1
Jeremy <jeremydaniel.bouvier@gmail.com>
2021-01-01 21:31:14 -0500

    Update MainActivity.kt, activity_main.xml, and strings.xml

--- app/src/main/java/com/example/android_lab_02/MainActivity.kt
+++ app/src/main/java/com/example/android_lab_02/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.android_lab_02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+class Mai
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+        val toast = Toast.makeText(this, "Dice Rolled! result is $diceRoll", Toast.LENGTH_SHORT)
+        toast.show()
+    }
+}
+
+/**
+ * Roll the dice and update the screen with the result.
+ */
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
