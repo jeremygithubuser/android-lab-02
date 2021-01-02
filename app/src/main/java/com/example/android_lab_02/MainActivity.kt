639b362f4359b1c84aefb4d2f4c082274e44f6be
Jeremy <jeremydaniel.bouvier@gmail.com>
2021-01-02 14:08:48 -0500

    Update MainActivity.kt, dice_1.png, and 6 more files...

--- app/src/main/java/com/example/android_lab_02/MainActivity.kt
+++ app/src/main/java/com/example/android_lab_02/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android_lab_02
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -15,17 +16,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { rollDice() }
+        setDefaultImage()
+        setupButton()
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         val resultTextView: TextView = findViewById(R.id.textView)
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
         resultTextView.text = diceRoll.toString()
         val toast = Toast.makeText(this, "Dice Rolled! result is $diceRoll", Toast.LENGTH_SHORT)
         toast.show()
+    }
+    private fun setupButton(){
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun setDefaultImage(){
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_1)
     }
 }
 
